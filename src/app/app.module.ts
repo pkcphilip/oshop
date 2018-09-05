e7b6f3eeb9622404e4f8ee3c7e96ae3f2acdf83c
Philip Pang <pkcphilip@outlook.com>
2018-09-05 16:56:48 +0800

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { environment } from './../environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2'; 
+import { AngularFireDatabaseModule } from 'angularfire2/database'; 
+import { AngularFireAuthModule } from 'angularfire2/auth'; 
+
+import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BsNavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
